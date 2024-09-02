e849724718edc1aed639450bc89253232bec518b
KishanPatel <kishan@nexgits.com>
2024-09-02 18:51:24 +0530

    Update urls.py

--- abhfl_1/urls.py
+++ abhfl_1/urls.py
@@ -19,6 +19,6 @@ from django.urls import path , include
 
 urlpatterns = [
     # path("admin/", admin.site.urls),
-    path("", include("salespitch_api1.urls")),
+    path("sales/", include("salespitch_api1.urls")),
 
 ]
