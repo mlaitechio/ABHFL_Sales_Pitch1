8731b8abd89d4281196aed354b127c76f1dc0d23
AakashJyoti <akjyoti572@gmail.com>
2024-12-21 19:07:02 +0530

    url.py update

--- salespitch/urls.py
+++ salespitch/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import my_view , HistoryAPIView , NewChatAPIView , ChatAPIView , StoreChat
+from .views import my_view , HistoryAPIView , NewChatAPIView , ChatAPIView , StoreChat , BookmarkMessage , RenameSessionAPIView
 
 urlpatterns = [
-    path('', my_view, name='my_view'),  
+  path('', my_view, name='my_view'),  
     path('history/', HistoryAPIView.as_view(), name='history'),
     path('new_chat/', NewChatAPIView.as_view(), name='new_chat'),
     path('chat/', ChatAPIView.as_view(), name='chat'),
     path('store/', StoreChat.as_view(), name='store_chat'),
+    path('bookmark/', BookmarkMessage.as_view(), name='bookmark'),
+    path('rename_session/', RenameSessionAPIView.as_view(), name='rename_session'),
 ]
