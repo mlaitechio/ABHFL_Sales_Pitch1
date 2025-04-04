824443b920e49846e570cc6788ac7842816f12d0
KishanPatel20 <kishanpatel6770@gmail.com>
2025-04-04 12:11:13 +0530

    UI update

---
+++ assets/useQuery-C8gTsDxK.js
@@ -0,0 +1,1 @@
+import{f as u}from"./index-CIIj6-ie.js";const y=u(e=>({query:"",updateQuery:r=>e({query:r}),clearQuery:()=>e({query:""})}));export{y as u};
---
+++ assets/useSidebar-B2UF2-SF.js
@@ -0,0 +1,1 @@
+import{f as a}from"./index-CIIj6-ie.js";const s=a(e=>({state:!1,showSidebar:()=>e({state:!0}),hideSidebar:()=>e({state:!1})}));export{s as u};
